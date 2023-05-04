2f2f8461476e15430756f35ae1c0af43c0465a89
matiasbastino <matiasbastino@gmail.com>
2023-05-04 15:43:40 -0300

    banner

--- frontEndAP/src/app/app.module.ts
+++ frontEndAP/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BannerComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
