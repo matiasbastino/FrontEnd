61e4e1a6a1f845d7d515026304ee2f7a9dca071e
matiasbastino <matiasbastino@gmail.com>
2023-05-06 14:11:35 -0300

    rutas

--- frontEndAP/src/app/app-routing.module.ts
+++ frontEndAP/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './componentes/index/index.component';
 import { Pagina404Component } from './componentes/pagina404/pagina404.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 
 const routes: Routes = [
-  {path: '', component: IndexComponent},
-  {path: '**', component: Pagina404Component}
+  { path: 'porfolio', component: IndexComponent },
+  { path: 'iniciarSesion', component: LoginComponent },
+  { path: '**', component: Pagina404Component }
 ];
 
 @NgModule({
+    declarations: [],
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
