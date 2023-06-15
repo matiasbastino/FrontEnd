ef219912be94fbddb9f9c008245b594fca6f4b7e
matiasbastino <matiasbastino@gmail.com>
2023-06-14 23:03:45 -0300

    rompi todo

--- frontEndAP/src/app/componentes/experiencia/experiencia.component.ts
+++ frontEndAP/src/app/componentes/experiencia/experiencia.component.ts
@@ -2,25 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { DatosService } from 'src/app/servicios/datos.service';
 import { Experiencia } from 'src/app/model/experiencia';
 import { ExperienciaService } from 'src/app/servicios/experiencia.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.css']
 })
-export class ExperienciaComponent implements OnInit {
-  experiencias: Experiencia[]=[];
 
-  constructor(private sExperiencia: ExperienciaService) {  }
-  ngOnInit(): void {
-    this.cargarExperiencia();
-    //if(this.tokenService.getToken()){
+export class ExperienciaComponent {
 
-    //}
-    throw new Error('Method not implemented.');
-  }
-
-    cargarExperiencia(): void{
-      this.sExperiencia.lista().subscribe(data=> {this.experiencias=data});
-    }
-  }
- 
+}
