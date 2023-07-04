42ae73661b3ff57a628f7636ad5055dd546b87d1
matiasbastino <matiasbastino@gmail.com>
2023-07-03 22:20:30 -0300

    back end 2

--- backEndAP/src/main/java/com/porfoliomatiasbastinoAP/matiasbastinoAP/Interface/IPersonaService.java
+++ backEndAP/src/main/java/com/porfoliomatiasbastinoAP/matiasbastinoAP/Interface/IPersonaService.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.porfoliomatiasbastinoAP.matiasbastinoAP.Interface;
 
-/**
- *
- * @author abecedario
- */
+import com.porfoliomatiasbastinoAP.matiasbastinoAP.Entity.Persona;
+import java.util.List;
+
 public interface IPersonaService {
+    //Traer una lista de persona
+    public List<Persona> getPersona();
     
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto pero lo buscamos por id
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
 }
